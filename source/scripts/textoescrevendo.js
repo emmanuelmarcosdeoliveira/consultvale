1e33eb9929c4eeeb6c5a5c21842d805971ca3efc
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-08-28 11:22:29 -0300

    :lipstick: Alteração Layout

--- source/scripts/textoescrevendo.js
+++ source/scripts/textoescrevendo.js
@@ -2,7 +2,7 @@
 const el = document.querySelector("#textoescrevendo");
 const text =
   "Soluções para diversos tipos de burocracias com preciso conhecimento técnico, resultados satisfatórios e um ótimo custo benefício.";
-const interval = 80;
+const interval = 40;
 function showText(el, text, interval) {
   const char = text.split("").reverse();
 
